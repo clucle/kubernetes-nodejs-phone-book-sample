a2a241d47578842a163669afdf08f2f97350e2a1
clucle <wjdenwls123@gmail.com>
2023-03-24 01:18:25 +0900

    feat: add login form
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const Coin = require('../Models/User.model');
 
-exports.homePage = async function(req, res) {
-    if (req.session && req.session.user) { 
+exports.homePage = async function (req, res) {
+    if (req.session && req.session.user) {
         let user = await User.findById(req.session.user);
-        
+
         /*
         res.render("pages/home", {
             name: user.first_name + ' ' + user.last_name,
@@ -19,11 +19,15 @@ exports.homePage = async function(req, res) {
     }
 }
 
-exports.loginPage = function(req,res){
-    if (req.session && req.session.user) { 
-       // Redirect to log out.
-       res.redirect("/logout");
-   }else{
+exports.loginPage = function (req, res) {
+    if (req.session && req.session.user) {
+        // Redirect to log out.
+        res.redirect("/logout");
+    } else {
         res.sendFile(path.join(__dirname, "../views", "login.html"));
-   }
+    }
+}
+
+exports.doLogin = function (req, res) {
+    console.log("do login");
 }
--- src/routes/User.route.js
+++ src/routes/User.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {homePage, loginPage} = require("../controllers/User.controller");
+const {homePage, loginPage, doLogin} = require("../controllers/User.controller");
 
 router.get("/", homePage);
 
 router.get("/login", loginPage);
+
+router.post("/login", doLogin);
 
 module.exports = router;
